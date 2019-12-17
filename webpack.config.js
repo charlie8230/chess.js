8062dbd0ced6dc400d8ead96ad517e0a63b218b7
Carlos <carlos@moranjr.com>
2019-12-16 19:30:11 -0500

    prep

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './chess.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'chess26.js',
+    filename: 'index.js',
     library: 'Chess',
     libraryTarget: 'umd'
   },
